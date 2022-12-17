f2fa6b031c02fb133d8e2acb3a998dff6c9c28a7
Tang <tangllty@163.com>
2022-12-17 18:30:45 +0800

    添加路由配置

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <RouterView />
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+</style>
---
+++ src/router/index.ts
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+
+const routes:Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    component: () => import('@/views/index.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
