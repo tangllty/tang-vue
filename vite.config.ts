a210a2a2790076ecbe8878afd826bc4b63b52661
Tang <tangllty@163.com>
2022-12-17 18:29:27 +0800

    配置项目别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // 配置项目别名
+  resolve: {
+    alias: {
+      '@': path.resolve('src'),
+    }
+  }
 })
