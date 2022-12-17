0c1f28d0c6be20a1158f141cd14a26fc7f5f1caa
Tang <tangllty@163.com>
2022-12-17 18:30:04 +0800

    自定义样式

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
-import router from "./router";
+import router from "./router"
+
+// 自定义样式
+import './styles/index.scss'
 
 const app = createApp(App)
 
 app
-    .use(router)
-    .mount('#app')
+  .use(router)
+  .mount('#app')
