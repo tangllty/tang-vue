44d053555bbe72b78603c9dd41d246e606fcc41a
Tang <tangllty@163.com>
2022-12-18 22:02:43 +0800

    添加菜单栏

---
+++ src/layout/components/Sidebar/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <el-scroller always = "true">
+    <el-menu
+      :default-active="activeMenu"
+      router
+      unique-opened
+    >
+      <el-menu-item index="/index">
+        <el-icon><House /></el-icon>
+        <span>首页</span>
+      </el-menu-item>
+      <el-sub-menu index="/system">
+        <template #title>
+          <el-icon><Setting /></el-icon>
+          <span>系统管理</span>
+        </template>
+        <el-menu-item index="/system/user">用户管理</el-menu-item>
+        <el-menu-item index="/system/dept">部门管理</el-menu-item>
+        <el-menu-item index="/system/menu">菜单管理</el-menu-item>
+        <el-menu-item index="/system/role">角色管理</el-menu-item>
+        <el-sub-menu index="/system/log">
+          <template #title>日志管理</template>
+          <el-menu-item index="/system/log/login">登陆日志</el-menu-item>
+          <el-menu-item index="/system/log/oper">操作日志</el-menu-item>
+        </el-sub-menu>
+      </el-sub-menu>
+    </el-menu>
+  </el-scroller>
+</template>
+
+<script lang="ts" setup>
+import { House, Setting, Histogram } from '@element-plus/icons-vue'
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+const activeMenu = computed(() => {
+  const { meta, path } = route
+  if (meta.activeMenu) {
+    return meta.activeMenu as string
+  }
+  return path
+})
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/layout/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <el-container class="app-wrapper">
+    <el-aside width="200px" class="sidebar-container">
+      <el-header style="background-color: aliceblue;">
+        Aside-header
+      </el-header>
+      <Sidebar class="sidebar" />
+    </el-aside>
+    <el-container class="app-container">
+      <el-header class="header-container" style="background-color: azure;">
+        Header
+      </el-header>
+      <el-main class="main-container">
+        <RouterView />
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script lang="ts" setup>
+import Sidebar from '@/layout/components/Sidebar/index.vue'
+</script>
+
+<style lang="scss" scoped>
+</style>
