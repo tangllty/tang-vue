fc2a0a24ce67e0d33f7a3503b266553e7190b5a4
Tang <tangllty@163.com>
2022-12-18 22:01:26 +0800

    添加Element Plus

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from "./router"
 
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 // 自定义样式
 import './styles/index.scss'
 
@@ -9,4 +12,5 @@ const app = createApp(App)
 
 app
   .use(router)
+  .use(ElementPlus)
   .mount('#app')
