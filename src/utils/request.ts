01dfd24ca758d9e77c1c7b4cf00b69914ea9d859
Tang <tangllty@163.com>
2022-12-18 22:03:30 +0800

    封装axios

---
+++ src/utils/request.ts
@@ -0,0 +1,42 @@
+import axios, { AxiosResponse } from 'axios'
+import { ElMessage } from 'element-plus';
+
+const service = axios.create({
+  baseURL: import.meta.env.VITE_APP_BASE_API,
+  timeout: 20000,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+
+// 添加请求拦截器
+service.interceptors.request.use(function (config) {
+  // 在发送请求之前做些什么
+  return config;
+}, function (error) {
+  // 对请求错误做些什么
+  return Promise.reject(error);
+});
+
+// 添加响应拦截器
+service.interceptors.response.use((response: AxiosResponse) => {
+  const { code, msg } = response.data
+  if (code === 200) {
+    ElMessage({
+      message: msg,
+      type: 'success'
+    })
+  }
+
+
+  return response.data;
+}, (error: any) => {
+  ElMessage({
+    message: error,
+    type: 'error'
+  })
+
+  return Promise.reject(error);
+});
+
+export default service
