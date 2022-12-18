ee49e96f1240467aaf6123d6e0bb505cdaee8062
Tang <tangllty@163.com>
2022-12-18 22:02:07 +0800

    添加axios

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,30 @@
-import { defineConfig } from 'vite'
+import { ConfigEnv, defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
+
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  // 配置项目别名
-  resolve: {
-    alias: {
-      '@': path.resolve('src'),
+export default ({ mode }:ConfigEnv) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return defineConfig ({
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': path.resolve('src'),
+      }
+    },
+    server: {
+      port: Number(env.VITE_APP_PORT),
+      open: false,
+      proxy: {
+        [env.VITE_APP_BASE_API]: {
+          target: 'http://127.0.0.1:8080',
+          changeOrigin: true,
+          rewrite: (path) => path.replace('^' + env.VITE_APP_BASE_API, '')
+        }
+      }
     }
-  }
-})
+  })
+}
