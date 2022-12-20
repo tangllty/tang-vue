c60203d79258a2b3d16dcbec1229988ebdd50b22
Tang <tangllty@163.com>
2022-12-21 03:52:42 +0800

    element 分页本地化

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import router from "./router"
 
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import zhLocale from 'element-plus/lib/locale/lang/zh-cn'
 
 // 自定义样式
 import './styles/index.scss'
@@ -12,5 +13,18 @@ const app = createApp(App)
 
 app
   .use(router)
-  .use(ElementPlus)
+  .use(ElementPlus, {
+    locale: {
+      el: {
+        // 整体覆盖
+        ...zhLocale.el,
+        pagination: {
+          pagesize: '条/页',
+          total: `共 {total} 条`,
+          goto: '前往第',
+          pageClassifier: '页'
+        },
+      }
+    }
+  })
   .mount('#app')
