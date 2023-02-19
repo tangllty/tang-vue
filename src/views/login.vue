84fe169727a0d9f86875d9e77cc5becb49484ab4
Tang <tangllty@163.com>
2023-02-20 00:15:36 +0800

    美化样式

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,75 +1,96 @@
 <template>
-    <div>
-      <el-tabs v-model="activeName" @tab-click="handleClick">
-        <el-tab-pane label="用户名密码" name="username">
-          <el-form
-            ref="loginRuleFormRef"
-            :model="loginForm"
-            status-icon
-            :rules="loginRules"
-            label-width="120px"
-            class="demo-ruleForm"
-          >
-            <el-form-item label="用户名" prop="username">
-              <el-input
-                v-model="loginForm.username"
-                type="text"
-                autocomplete="off"
-              />
-            </el-form-item>
-            <el-form-item label="密码" prop="password">
-              <el-input
-                v-model="loginForm.password"
-                type="password"
-                autocomplete="off"
-              />
-            </el-form-item>
-            <el-form-item>
-              <el-button
-                type="primary"
-                @click="submitForm(loginRuleFormRef)"
-              >登陆</el-button>
-              <el-button
-                @click="resetForm(loginRuleFormRef)"
-              >Reset</el-button>
-            </el-form-item>
-          </el-form>
-        </el-tab-pane>
-        <el-tab-pane label="邮箱密码" name="email">
-          <el-form
-            ref="loginRuleFormRef"
-            :model="loginForm"
-            status-icon
-            :rules="loginRules"
-            label-width="120px"
-            class="demo-ruleForm"
-          >
-            <el-form-item label="邮箱" prop="email">
-              <el-input
-                v-model="loginForm.email"
-                type="text"
-                autocomplete="off"
-              />
-            </el-form-item>
-            <el-form-item label="密码" prop="password">
-              <el-input
-                v-model="loginForm.password"
-                type="password"
-                autocomplete="off"
-              />
-            </el-form-item>
-            <el-form-item>
-              <el-button
-                type="primary"
-                @click="submitForm(loginRuleFormRef)"
-              >登陆</el-button>
-              <el-button
-                @click="resetForm(loginRuleFormRef)"
-              >Reset</el-button>
-            </el-form-item>
-          </el-form>
-        </el-tab-pane>
-      </el-tabs>
+    <div class="login-container">
+      <el-card class="login">
+        <template #header>
+          <div class="login-header">
+            <span>权限管理系统</span>
+          </div>
+        </template>
+        <el-tabs v-model="activeName" @tab-click="handleClick">
+          <el-tab-pane label="用户名密码" name="username">
+            <el-form
+              ref="loginRuleFormRef"
+              :model="loginForm"
+              status-icon
+              :rules="loginRules"
+              label-width="120px"
+              class="login-form"
+            >
+              <el-form-item label="用户名" prop="username">
+                <el-input
+                  v-model="loginForm.username"
+                  type="text"
+                  autocomplete="off"
+                  @keyup.enter="submitForm(loginRuleFormRef)"
+                  placeholder="用户名"
+                  :prefix-icon="User"
+                />
+              </el-form-item>
+              <el-form-item label="密码" prop="password">
+                <el-input
+                  v-model="loginForm.password"
+                  type="password"
+                  autocomplete="off"
+                  @keyup.enter="submitForm(loginRuleFormRef)"
+                  show-password
+                  placeholder="密码"
+                  :prefix-icon="Lock"
+                />
+              </el-form-item>
+              <el-form-item>
+                <el-button
+                  type="primary"
+                  @click="submitForm(loginRuleFormRef)"
+                >登陆</el-button>
+                <el-button
+                  @click="resetForm(loginRuleFormRef)"
+                >Reset</el-button>
+              </el-form-item>
+            </el-form>
+          </el-tab-pane>
+          <el-tab-pane label="邮箱密码" name="email">
+            <el-form
+              ref="loginRuleFormRef"
+              :model="loginForm"
+              status-icon
+              :rules="loginRules"
+              label-width="120px"
+              class="login-form"
+            >
+              <el-form-item label="邮箱" prop="email">
+                <el-input
+                  v-model="loginForm.email"
+                  type="text"
+                  autocomplete="off"
+                  @keyup.enter="submitForm(loginRuleFormRef)"
+                  placeholder="用户名"
+                  :prefix-icon="Message"
+                />
+              </el-form-item>
+              <el-form-item label="密码" prop="password">
+                <el-input
+                  v-model="loginForm.password"
+                  type="password"
+                  autocomplete="off"
+                  @keyup.enter="submitForm(loginRuleFormRef)"
+                  show-password
+                  placeholder="密码"
+                  :prefix-icon="Lock"
+                />
+              </el-form-item>
+              <el-form-item>
+                <el-button
+                  type="primary"
+                  @click="submitForm(loginRuleFormRef)"
+                >登陆</el-button>
+                <el-button
+                  @click="resetForm(loginRuleFormRef)"
+                >Reset</el-button>
+              </el-form-item>
+            </el-form>
+          </el-tab-pane>
+        </el-tabs>
+      </el-card>
     </div>
 </template>
 
@@ -77,6 +98,7 @@
 import { reactive, ref, toRefs } from 'vue'
 import router from '@/router'
 import { ElButton, ElForm, ElFormItem, ElInput, ElTabPane, ElTabs, FormInstance, FormRules, TabsPaneContext } from 'element-plus'
+import { User, Lock, Message } from '@element-plus/icons-vue'
 import { LoginForm } from '@/api/auth/types'
 import { useUserStore } from "@/store/modules/user"
 
@@ -85,7 +107,7 @@ const userStore = useUserStore()
 const state = reactive({
     loginForm: {
       username: 'admin',
-      email: 'tangllty@163.com',
+      email: 'admin@163.com',
       password: '123456',
       loginType: 'username'
     } as LoginForm,
@@ -140,4 +162,25 @@ const resetForm = (formEl: FormInstance | undefined) => {
 </script>
 
 <style lang="scss" scoped>
+.login-container {
+    display: flex;
+    justify-content: center;
+
+  .login {
+    width: 520px;
+    margin-top: 150px;
+
+    .login-header {
+      text-align: center;
+      font-size: 18px;
+    }
+
+    .login-form {
+
+      .el-input {
+        width: 80%;
+      }
+    }
+  }
+}
 </style>
