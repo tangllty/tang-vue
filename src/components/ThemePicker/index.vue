90186e1a370f1cb063117d4f1b706a3aa5fafc1e
Tang <tangllty@163.com>
2023-08-15 10:11:55 +0800

    预定义颜色

--- src/components/ThemePicker/index.vue
+++ src/components/ThemePicker/index.vue
@@ -1,6 +1,7 @@
 <template>
   <el-color-picker
     v-model="theme"
+    :predefine="predefineColors"
     @active-change="handleActiveChange"
     @change="handleChange"
   />
@@ -17,13 +18,42 @@ const proxy: any = getProxy()
 const settingStore = useSettingStore()
 
 const state = reactive({
-  theme: settingStore.theme
+  theme: settingStore.theme,
+  predefineColors: [
+    '#409eff',
+    '#00ced1',
+    '#1abc9c',
+    '#16a085',
+    '#90ee90',
+    '#2ecc71',
+    '#27ae60',
+    '#3498db',
+    '#2980b9',
+    '#722ed1',
+    '#9b59b6',
+    '#8e44ad',
+    '#34495e',
+    '#2c3e50',
+    '#f1c40f',
+    '#f39c12',
+    '#e67e22',
+    '#d35400',
+    '#fd726d',
+    '#e74c3c',
+    '#c0392b',
+    '#ecf0f1',
+    '#bdc3c7',
+    '#95a5a6',
+    '#7f8c8d'
+  ]
 })
 
 const {
-  theme
+  theme,
+  predefineColors
 } = toRefs(state)
 
+// 面板中显示的颜色发生改变时触发
 const handleActiveChange = (color: string | null) => {
   if (!color) {
     removeTheme()
@@ -32,6 +62,7 @@ const handleActiveChange = (color: string | null) => {
   setTheme(color)
 }
 
+// 绑定值发生改变时触发
 const handleChange = (color: string | null) => {
   if (!color) {
     removeTheme()
