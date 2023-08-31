638c73b85d53be740a5b70d2b34d859d530278be
Tang <tangllty@163.com>
2023-08-31 09:42:40 +0800

    优化样式，简化代码

--- src/views/monitor/server/index.vue
+++ src/views/monitor/server/index.vue
@@ -7,24 +7,12 @@
             <span>系统信息</span>
           </div>
         </template>
-        <div class="el-table el-table--enable-row-hover el-table--medium">
-          <table>
-            <thead>
-              <th class="el-table__cell is-leaf"><div class="cell">主机名</div></th>
-              <th class="el-table__cell is-leaf"><div class="cell">系统名称</div></th>
-              <th class="el-table__cell is-leaf"><div class="cell">系统架构</div></th>
-              <th class="el-table__cell is-leaf"><div class="cell">系统版本</div></th>
-            </thead>
-            <tbody>
-              <tr>
-                <td class="el-table__cell is-leaf"><div class="cell">{{ os.hostName }}</div></td>
-                <td class="el-table__cell is-leaf"><div class="cell">{{ os.name }}</div></td>
-                <td class="el-table__cell is-leaf"><div class="cell">{{ os.arch }}</div></td>
-                <td class="el-table__cell is-leaf"><div class="cell">{{ os.version }}</div></td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+        <el-table :data="[os]">
+          <el-table-column prop="hostName" label="主机名" />
+          <el-table-column prop="name" label="系统名称" />
+          <el-table-column prop="arch" label="系统架构" />
+          <el-table-column prop="version" label="系统版本" />
+        </el-table>
       </el-card>
     </el-col>
     <el-col :span="24" class="mt-15">
@@ -34,24 +22,12 @@
             <span>Java 信息</span>
           </div>
         </template>
-        <div class="el-table el-table--enable-row-hover el-table--medium">
-          <table>
-            <thead>
-              <th class="el-table__cell is-leaf"><div class="cell">Java 路径</div></th>
-              <th class="el-table__cell is-leaf"><div class="cell">Java 版本</div></th>
-              <th class="el-table__cell is-leaf"><div class="cell">Jvm 名称</div></th>
-              <th class="el-table__cell is-leaf"><div class="cell">项目路径</div></th>
-            </thead>
-            <tbody>
-              <tr>
-                <td class="el-table__cell is-leaf"><div class="cell">{{ java.javaHome }}</div></td>
-                <td class="el-table__cell is-leaf"><div class="cell">{{ java.javaVersion }}</div></td>
-                <td class="el-table__cell is-leaf"><div class="cell">{{ java.jvmName }}</div></td>
-                <td class="el-table__cell is-leaf"><div class="cell">{{ java.projectHome }}</div></td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+        <el-table :data="[java]">
+          <el-table-column prop="javaHome" label="Java 路径" />
+          <el-table-column prop="javaVersion" label="Java 版本" />
+          <el-table-column prop="jvmName" label="Jvm 名称" />
+          <el-table-column prop="projectHome" label="项目路径" />
+        </el-table>
       </el-card>
     </el-col>
     <el-col :span="24" class="mt-15">
@@ -61,24 +37,12 @@
             <span>内存信息</span>
           </div>
         </template>
-        <div class="el-table el-table--enable-row-hover el-table--medium">
-          <table>
-            <thead>
-              <th class="el-table__cell is-leaf"><div class="cell">总内存</div></th>
-              <th class="el-table__cell is-leaf"><div class="cell">已用内存</div></th>
-              <th class="el-table__cell is-leaf"><div class="cell">剩余内存</div></th>
-              <th class="el-table__cell is-leaf"><div class="cell">内存使用率</div></th>
-            </thead>
-            <tbody>
-              <tr>
-                <td class="el-table__cell is-leaf"><div class="cell">{{ memory.total }}</div></td>
-                <td class="el-table__cell is-leaf"><div class="cell">{{ memory.used }}</div></td>
-                <td class="el-table__cell is-leaf"><div class="cell">{{ memory.free }}</div></td>
-                <td class="el-table__cell is-leaf"><div class="cell">{{ memory.usage }}</div></td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+        <el-table :data="[memory]">
+          <el-table-column prop="total" label="总内存" />
+          <el-table-column prop="used" label="已用内存" />
+          <el-table-column prop="free" label="剩余内存" />
+          <el-table-column prop="usage" label="内存使用率" />
+        </el-table>
       </el-card>
     </el-col>
     <el-col :span="24" class="mt-15">
@@ -88,24 +52,12 @@
             <span>Jvm 内存信息</span>
           </div>
         </template>
-        <div class="el-table el-table--enable-row-hover el-table--medium">
-          <table>
-            <thead>
-              <th class="el-table__cell is-leaf"><div class="cell">总内存</div></th>
-              <th class="el-table__cell is-leaf"><div class="cell">已用内存</div></th>
-              <th class="el-table__cell is-leaf"><div class="cell">剩余内存</div></th>
-              <th class="el-table__cell is-leaf"><div class="cell">内存使用率</div></th>
-            </thead>
-            <tbody>
-              <tr>
-                <td class="el-table__cell is-leaf"><div class="cell">{{ jvmMemory.total }}</div></td>
-                <td class="el-table__cell is-leaf"><div class="cell">{{ jvmMemory.used }}</div></td>
-                <td class="el-table__cell is-leaf"><div class="cell">{{ jvmMemory.free }}</div></td>
-                <td class="el-table__cell is-leaf"><div class="cell">{{ jvmMemory.usage }}</div></td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+        <el-table :data="[jvmMemory]">
+          <el-table-column prop="total" label="总内存" />
+          <el-table-column prop="used" label="已用内存" />
+          <el-table-column prop="free" label="剩余内存" />
+          <el-table-column prop="usage" label="内存使用率" />
+        </el-table>
       </el-card>
     </el-col>
     <el-col :span="24" class="mt-15">
@@ -115,30 +67,15 @@
             <span>CPU 信息</span>
           </div>
         </template>
-        <div class="el-table el-table--enable-row-hover el-table--medium">
-          <table>
-            <thead>
-              <th class="el-table__cell is-leaf"><div class="cell">名称</div></th>
-              <th class="el-table__cell is-leaf"><div class="cell">核心数</div></th>
-              <th class="el-table__cell is-leaf"><div class="cell">总的使用率</div></th>
-              <th class="el-table__cell is-leaf"><div class="cell">系统使用率</div></th>
-              <th class="el-table__cell is-leaf"><div class="cell">用户使用率</div></th>
-              <th class="el-table__cell is-leaf"><div class="cell">当前等待率</div></th>
-              <th class="el-table__cell is-leaf"><div class="cell">当前空闲率</div></th>
-            </thead>
-            <tbody>
-              <tr>
-                <td class="el-table__cell is-leaf"><div class="cell">{{ cpu.name }}</div></td>
-                <td class="el-table__cell is-leaf"><div class="cell">{{ cpu.cpuNum }}</div></td>
-                <td class="el-table__cell is-leaf"><div class="cell">{{ cpu.total }}</div></td>
-                <td class="el-table__cell is-leaf"><div class="cell">{{ cpu.sys }}</div></td>
-                <td class="el-table__cell is-leaf"><div class="cell">{{ cpu.user }}</div></td>
-                <td class="el-table__cell is-leaf"><div class="cell">{{ cpu.wait }}</div></td>
-                <td class="el-table__cell is-leaf"><div class="cell">{{ cpu.free }}</div></td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+        <el-table :data="[cpu]">
+          <el-table-column prop="name" label="名称" width="320" />
+          <el-table-column prop="cpuNum" label="核心数" />
+          <el-table-column prop="total" label="总的使用率" />
+          <el-table-column prop="sys" label="系统使用率" />
+          <el-table-column prop="user" label="用户使用率" />
+          <el-table-column prop="wait" label="当前等待率" />
+          <el-table-column prop="free" label="当前空闲率" />
+        </el-table>
       </el-card>
     </el-col>
     <el-col :span="24" class="mt-15">
@@ -148,7 +85,7 @@
             <span>分区信息</span>
           </div>
         </template>
-        <el-table :data="fileStores" class="file-stores">
+        <el-table :data="fileStores">
           <el-table-column prop="name" label="分区名" />
           <el-table-column prop="type" label="文件系统" />
           <el-table-column prop="mountPoint" label="挂载点" />
@@ -214,16 +151,7 @@ onMounted(async () => {
 </script>
 
 <style lang="scss" scoped>
-table {
-  border-spacing: 0;
-  width: 100%;
-
-  td, th {
-    padding: 0;
-  }
-}
-
-.file-stores {
-  width: 100%;
+.card-header {
+  font-weight: bold;
 }
 </style>
