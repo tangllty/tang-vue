0de9cca99f2d0b794e2336b005b960b019f935ee
Tang <tangllty@163.com>
2023-12-06 09:53:16 +0800

    自定义全局属性

--- src/vue.d.ts
+++ src/vue.d.ts
@@ -2,10 +2,10 @@ import { getDicts } from '@/utils/dict'
 import { download } from '@/utils/download'
 import { getFullPath } from '@/utils/path'
 import webSocketService from '@/utils/websocket'
-import i18n from './locales'
+import i18n from '@/locales'
 
-// 在 Vue 实例上扩展自定义属性
-declare module '@vue/runtime-core' {
+// 在 Vue 实例上扩展自定义属性 https://vuejs.org/guide/typescript/options-api.html#augmenting-global-properties
+declare module 'vue' {
   interface ComponentCustomProperties {
     $dict: getDicts
     $download: download
