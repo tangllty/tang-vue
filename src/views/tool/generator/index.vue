06f766cb0c2faad39e4212a29e4b13cc0eda106e
Tang <tangllty@163.com>
2024-01-14 12:27:22 +0800

    添加表格字段

--- src/views/tool/generator/index.vue
+++ src/views/tool/generator/index.vue
@@ -113,6 +113,11 @@
           align="center"
         />
         <el-table-column
+          prop="remark"
+          label="备注"
+          align="center"
+        />
+        <el-table-column
           label="操作"
           align="center"
           width="250"
