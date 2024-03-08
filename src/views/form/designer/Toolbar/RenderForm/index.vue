47fd0b8a011d6de44c64c704118ce3bd5118ed74
Tang <tangllty@163.com>
2024-03-08 15:32:01 +0800

    添加表单预览相关功能

--- src/views/form/designer/Toolbar/RenderForm/index.vue
+++ src/views/form/designer/Toolbar/RenderForm/index.vue
@@ -1,29 +1,99 @@
 <template>
   <el-form label-width="120px">
     <Render v-model="componentList" />
-
-    <!-- TODO 确定&取消按钮 -->
   </el-form>
+
+  <footer class="flex flex-justify-end pt-16">
+    <el-button type="primary" @click="handleFormData">获取数据</el-button>
+    <el-button type="primary" @click="handleFormReset">重置</el-button>
+    <el-button>关闭</el-button>
+  </footer>
+
+  <el-dialog
+    :title="formDataDialog.title"
+    v-model="formDataDialog.visible"
+    width="65%"
+    append-to-body
+  >
+    <el-button
+      type="primary"
+      link
+      :icon="DocumentCopy"
+      size="small"
+      class="fr"
+      @click="$copy(formData)"
+    >复制</el-button>
+    <Highlight :code="formData" />
+  </el-dialog>
 </template>
 
 <script lang="ts" setup>
+import { DocumentCopy } from '@element-plus/icons-vue'
+import { getProxy } from '@/utils/getCurrentInstance'
 import type { Component } from '../../types'
 import Render from './Render.vue'
 
+const proxy = getProxy()
+
 const state = reactive({
-  componentList: [] as Component[]
+  componentList: [] as Component[],
+  formData: '',
+  formDataDialog: {
+    title: '表单数据预览',
+    type: 'list',
+    visible: false
+  } as Dialog
 })
 
 const {
+  formData,
+  formDataDialog,
   componentList
 } = toRefs(state)
 
 const handleSet = (formComponentList: Component[]) => {
-  componentList.value = formComponentList
+  componentList.value = handleResetVModel(formComponentList)
+}
+
+const handleResetVModel = (formComponentList: Component[]): Component[] => {
+  return formComponentList.map(item => {
+    if (item.children) {
+      item.children = handleResetVModel(item.children)
+    }
+    return {
+      ...item,
+      vModel: null
+    }
+  })
 }
 
 const handleReset = () => {
   componentList.value = []
+}
+
+const handleFormReset = () => {
+  componentList.value = handleResetVModel(componentList.value)
+}
+
+const handleVModelData = (formComponentList: Component[]): { [key: string]: any } => {
+  return formComponentList.reduce((formData, item) => {
+    if (item.field) {
+      (formData as { [key: string]: any })[item.field] = item.vModel
+    }
+
+    if (item.children) {
+      Object.assign(formData, handleVModelData(item.children))
+    }
+
+    return formData
+  }, {})
+}
+
+const handleFormData = () => {
+  const formData = handleVModelData(componentList.value)
+  state.formData = JSON.stringify(formData, null, 2)
+  formDataDialog.value.visible = true
+  console.log(proxy)
 }
 
 defineExpose({
