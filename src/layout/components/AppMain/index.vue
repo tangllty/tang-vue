3800305a33200e23a25fc63ed80c97c8bd5f9ee8
Tang <tangllty@163.com>
2024-03-17 23:14:28 +0800

    优化过度效果，清除警告

--- src/layout/components/AppMain/index.vue
+++ src/layout/components/AppMain/index.vue
@@ -1,24 +1,13 @@
 <template>
-  <Transition name="slide-fade" mode="out-in">
-    <RouterView />
-  </Transition>
+  <RouterView v-slot="{ Component }">
+    <Transition name="slide-fade" mode="out-in">
+      <Component :is="Component" />
+    </Transition>
+  </RouterView>
 </template>
 
 <script lang="ts" setup>
 </script>
 
 <style lang="scss" scoped>
-.slide-fade-enter-active {
-  transition: all 0.25s ease-out;
-}
-
-.slide-fade-leave-active {
-  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
-}
-
-.slide-fade-enter-from,
-.slide-fade-leave-to {
-  transform: translateX(-20px);
-  opacity: 0;
-}
 </style>
