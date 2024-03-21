b535ff2e0451d763157b48ff72e4efb09280e883
Tang <tangllty@163.com>
2024-03-21 17:20:58 +0800

    隐藏横向滚动条

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -66,6 +66,10 @@ const fixedHeader = computed(() => settingStore.fixedHeader)
       height: $headerHeight;
       box-shadow: 0 2px 6px 2px rgba(0, 0, 0, .08);
     }
+
+    .main-container {
+      overflow-x: hidden;
+    }
   }
 }
 </style>
