c1f3ceecb5ed4025b410cf1a35e24196311a600e
Tang <tangllty@163.com>
2024-04-20 01:06:15 +0800

    Wrap MonacoEditor components in a div

--- src/views/editor/monaco/index.vue
+++ src/views/editor/monaco/index.vue
@@ -1,6 +1,8 @@
 <template>
-  <MonacoEditor v-model="js" :language="'javascript'" />
-  <MonacoEditor v-model="html" :language="'html'" />
+  <div>
+    <MonacoEditor v-model="js" :language="'javascript'" />
+    <MonacoEditor v-model="html" :language="'html'" />
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -9,4 +11,7 @@ const html = ref<string>(['<div>', '\t<h1>Hello world!</h1>', '</div>'].join('\n
 </script>
 
 <style lang="scss" scoped>
+div {
+  height: 100%;
+}
 </style>
