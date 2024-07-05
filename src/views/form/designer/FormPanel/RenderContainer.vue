1de8aa89eaa3a2272f1a0158944f5800cee43cd0
Tang <tangllty@163.com>
2024-07-05 09:10:46 +0800

    Move render container to a new file

---
+++ src/views/form/designer/FormPanel/RenderContainer.vue
@@ -0,0 +1,96 @@
+<template>
+  <el-row v-if="component.element === 'el-row'">
+    <el-col
+      v-for="(item, index) in component.children"
+      :key="index"
+      :span="item.span"
+      :class="{ 'active-item': activeItem === item }"
+      @click="handleActiveItem(item, $event)"
+    >
+      <NestedForm v-model="item.children" v-model:activeItem="activeItem" />
+    </el-col>
+  </el-row>
+  <el-card v-else-if="component.element === 'el-card'" :header="component.header">
+    <NestedForm v-model="component.children" v-model:activeItem="activeItem" />
+  </el-card>
+  <el-watermark
+    v-else-if="component.element === 'el-watermark'"
+    :content="component.watermark"
+    :class="{
+      'el-watermark__empty': !component.children || component.children.length === 0,
+    }"
+    class="el-watermark"
+  >
+    <NestedForm v-model="component.children" v-model:activeItem="activeItem" />
+  </el-watermark>
+</template>
+
+<script lang="ts" setup>
+import { getProxy } from '@/utils/getCurrentInstance'
+import type { Component } from '../types'
+import NestedForm from './NestedForm.vue'
+
+const proxy = getProxy()
+
+const props = defineProps({
+  modelValue: {
+    type: Object as () => Component,
+    default: null
+  },
+  activeItem: {
+    type: Object as PropType<Component>,
+    default: null
+  }
+})
+
+const component = computed({
+  get: () => props.modelValue,
+  set: val => proxy.$emit('update:modelValue', val)
+})
+
+const activeItem = computed<Component>({
+  get: () => props.activeItem,
+  set: val => proxy.$emit('update:activeItem', val)
+})
+
+const handleActiveItem = (item: Component, event: MouseEvent | null = null) => {
+  if (!event) return
+  event.preventDefault()
+  event.stopPropagation()
+  activeItem.value = item
+}
+</script>
+
+<style lang="scss" scoped>
+@import './index.scss';
+
+.el-row, .el-col {
+  min-height: 50px;
+  border: 1px dashed #336699;
+}
+
+.el-row {
+  padding: 2px;
+}
+
+.el-col {
+  padding: 0 8px;
+  position: relative;
+
+  .drag-handler {
+    left: -2px;
+  }
+}
+
+:deep(.el-card__body) {
+  min-height: 20px;
+}
+
+.el-watermark {
+  border: 1px dashed #336699;
+
+  &__empty {
+    height: 50px;
+  }
+}
+</style>
--- src/views/form/designer/FormPanel/RenderForm.vue
+++ src/views/form/designer/FormPanel/RenderForm.vue
@@ -2,38 +2,18 @@
   <RenderFormItem v-if="component.type === 'component'" v-model="component" />
 
   <template v-else-if="component.type === 'container' && component.children">
-    <el-row v-if="component.element === 'el-row'">
-      <el-col
-        v-for="(item, index) in component.children"
-        :key="index"
-        :span="item.span"
-        :class="{ 'active-item': activeItem === item }"
-        @click="handleActiveItem(item, $event)"
-      >
-        <NestedForm v-model="item.children" v-model:activeItem="activeItem" />
-      </el-col>
-    </el-row>
-    <el-card v-else-if="component.element === 'el-card'" :header="component.header">
-      <NestedForm v-model="component.children" v-model:activeItem="activeItem" />
-    </el-card>
-    <el-watermark
-      v-else-if="component.element === 'el-watermark'"
-      :content="component.watermark"
-      :class="{
-        'el-watermark__empty': component.children.length === 0,
-      }"
-      class="el-watermark"
-    >
-      <NestedForm v-model="component.children" v-model:activeItem="activeItem" />
-    </el-watermark>
+    <RenderContainer
+      v-model="component"
+      v-model:activeItem="activeItem"
+    />
   </template>
 </template>
 
 <script lang="ts" setup>
 import { getProxy } from '@/utils/getCurrentInstance'
 import type { Component } from '../types'
+import RenderContainer from './RenderContainer.vue'
 import RenderFormItem from './RenderFormItem.vue'
-import NestedForm from './NestedForm.vue'
 
 const proxy = getProxy()
 
@@ -57,45 +37,8 @@ const activeItem = computed<Component>({
   get: () => props.activeItem,
   set: val => proxy.$emit('update:activeItem', val)
 })
-
-const handleActiveItem = (item: Component, event: MouseEvent | null = null) => {
-  if (!event) return
-  event.preventDefault()
-  event.stopPropagation()
-  activeItem.value = item
-}
 </script>
 
 <style lang="scss" scoped>
 @import './index.scss';
-
-.el-row, .el-col {
-  min-height: 50px;
-  border: 1px dashed #336699;
-}
-
-.el-row {
-  padding: 2px;
-}
-
-.el-col {
-  padding: 0 8px;
-  position: relative;
-
-  .drag-handler {
-    left: -2px;
-  }
-}
-
-:deep(.el-card__body) {
-  min-height: 20px;
-}
-
-.el-watermark {
-  border: 1px dashed #336699;
-
-  &__empty {
-    height: 50px;
-  }
-}
 </style>
